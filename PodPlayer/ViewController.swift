70170d6b05cb8d8ad7130d9a4974e960afc506e6
Ivan Sliepov <slepow1985@gmail.com>
2020-09-02 17:46:30 +0300

    Initial Commit

---
+++ PodPlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PodPlayer
+//
+//  Created by Ivan Sliepov on 02.09.2020.
+//  Copyright © 2020 Ivan Sliepov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
